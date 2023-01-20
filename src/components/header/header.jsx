631b498fcdc5de5f941e942cb9f4a0ffa1c244d9
malish-undead-core <vsmolenskyi88@gmail.com>
2023-01-20 13:10:27 +0200

    fix

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="header__wrapper">
                 <h1 className="header__title">
                     <strong>Hi, my name is <em>Slava</em></strong><br />
-                    a frontend developer
+                    i'm a frontend developer
                 </h1>
                 <div className="header__text">
                     <p>with passion for learning and creating.</p>
